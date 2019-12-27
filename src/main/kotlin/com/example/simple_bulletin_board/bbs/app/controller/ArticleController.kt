d7582eac3ccfacfdf30dcacbbbbf1de1b353d198
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-27 14:56:17 +0900

    create controller

---
+++ src/main/kotlin/com/example/simple_bulletin_board/bbs/app/controller/ArticleController.kt
@@ -0,0 +1,28 @@
+package com.example.simple_bulletin_board.bbs.app.controller
+
+import com.example.simple_bulletin_board.bbs.domain.entity.Article
+import com.example.simple_bulletin_board.bbs.domain.repository.ArticleRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class ArticleController {
+
+    @Autowired
+    lateinit var articleRepository: ArticleRepository
+
+    @PostMapping
+    @ResponseBody
+    fun registerArticle(
+            @RequestParam name: String,
+            @RequestParam title: String,
+            @RequestParam contents: String,
+            @RequestParam articleKey: String
+    ): String {
+        articleRepository.save(Article(0, name, title, contents, articleKey))
+        return "Saved"
+    }
+}
